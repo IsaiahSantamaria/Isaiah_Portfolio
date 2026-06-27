87b266b93a609d700c4ae37de0ee14916b1123fd
IsaiahSantamaria <isaiahasant@gmail.com>
2026-06-27 16:16:03 -0400

    reverting config file and package.json file

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-  base: "https://isaiahsantamaria.github.io/Isaiah_Portfolio/",
+  base: "/Isaiah_Portfolio/",
 })
